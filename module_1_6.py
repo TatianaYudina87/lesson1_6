ccb9771f54462b8953877e22cd9f655fc818da5b
TatianaYudina87 <tanyamaz8@gmail.com>
2024-12-03 18:49:55 +0300

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -4,5 +4,5 @@ total_tasks = 12
 print(total_tasks)
 hours_spent = 1.5
 print(hours_spent)
-average_execution_time = (1.5/12)
+average_execution_time = (hours_spent/total_tasks)
 print(average_execution_time)
