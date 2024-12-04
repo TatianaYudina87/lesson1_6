044458c0b06ab105fcc8cac42edc5b1f6757237f
TatianaYudina87 <tanyamaz8@gmail.com>
2024-12-04 04:07:25 +0300

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -1,8 +1,5 @@
 course = "Python"
-print(course)
 total_tasks = 12
-print(total_tasks)
 hours_spent = 1.5
-print(hours_spent)
 average_execution_time = 0.125
-print(f'hours_spent {1.5}, total_tasks {12}')
+print(f'course {"Python"}, hours_spent {1.5}, total_tasks {12}, average_execution_time {0.125}')
