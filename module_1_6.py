8ac5e039d017db1fd1d301b429e953a2813494fe
TatianaYudina87 <tanyamaz8@gmail.com>
2024-12-05 10:33:38 +0300

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -1,5 +1,5 @@
 course = "Python"
 total_tasks = 12
 hours_spent = 1.5
-average_execution_time = 0.125
-print(f'course {"Python"}, hours_spent {1.5}, total_tasks {12}, average_execution_time {0.125}')
+average_execution_time = (hours_spent/total_tasks)
+print(f'Курс: {course}, всего задач: {total_tasks}, затрачено часов: {hours_spent}, среднее время выполнения: {average_execution_time} часа')
